2e1d2371725acaba7ec8a59b9b76d91eb9f413cf
hasanbasriokan <hasanbasriokan98@gmail.com>
2022-04-24 19:22:27 +0300

    İlk düzenleme

--- c_csytem.cpp
+++ c_csytem.cpp
@@ -1,5 +1,5 @@
-﻿// c_csytem.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
-//
+﻿//Hasan Basri OKAN
+//hasanbasriokan98@gmail.com
 
 #include <iostream>
 
@@ -8,30 +8,6 @@ using namespace std;
 int main()
 {
     cout << "Hello World!\n";
+    
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
-// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
-
-// Kullanmaya Başlama İpuçları: 
-//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
-//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
-//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
-//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
-//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
-//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
